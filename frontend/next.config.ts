e8b4cac01748ba5876b18188fb1d1e9fa9bd14a7
AdhishthanAshok <adhishthanashok@gmail.com>
2026-09-18 11:27:11 +0530

    added custom backend url in api.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    CUSTOM_API_URL: process.env.BACKEND_URL,
+  },
   allowedDevOrigins: ["192.168.1.7", "http://localhost:3000", "https://ai-inventory-nu.vercel.app"],
   images: {
     remotePatterns: [
